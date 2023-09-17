603af8852211586bf74aa826a0d363de1491ac86
MEVINDANI <mevindani14@gmail.com>
2023-09-17 15:51:35 +0530

    added individual country page

--- src/CountryView.js
+++ src/CountryView.js
@@ -1,8 +1,66 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import './Country.css'
+
 
 const CountryView = () => {
+
+    const params = useParams()
+    const [data, setData] = useState([])
+    const [filteredData, setFilteredData] = useState([]);
+
+    const getData = async () => {
+        const result = await fetch('https://restcountries.com/v3.1/all')
+            .then(result => result.json())
+            .then(d => setFilteredData(d.filter(item => item.name.common.toLowerCase().includes(params.id.toLowerCase().trim()))))
+
+    }
+
+    useEffect(() => {
+        getData()
+    }, [])
+
+
+    console.log(filteredData)
+
     return (
-        <div>CountryView</div>
+        filteredData.map((item, index) => {
+            const firstCurrency = Object.values(item.currencies)[0];
+            const language = Object.values(item.languages)
+            return (
+                <>
+                    <div className='count-cont'>
+                        <div className='count-item'>
+                            <div className='count-flag-cont'><img src={item.flags.png} alt="" /></div>
+                            <div className='count-item-cont'>
+                                <div className='count-name'><strong>{item.name.common}</strong></div>
+                                <div><p>{item.flags.alt}</p></div>
+                                <div className='count-continent'>Continent: <strong>{item.continents[0]}</strong></div>
+                                <div className='count-population'>Population: <strong>{item.population}</strong></div>
+                                <div className='count-currency'>Currency: <strong>{firstCurrency.name}</strong></div>
+                                <div className='count-lang'>Language: <strong>{language[0]}</strong></div>
+                            </div>
+                        </div>
+                        <div className='count-other'>
+                            <div className='c-1'>
+                                <div>Capital: <strong>{item.capital[0]}</strong></div>
+                                <div>Official Name: <strong>{item.name.official}</strong></div>
+                                <div>Independent: <strong>{item.independent ? 'Independent Country' : 'Not Independent'}</strong></div>
+                                <div>Region: <strong>{item.region}</strong></div>
+                                <div>startOfWeek: <strong>{item.startOfWeek}</strong></div>
+                                <div>status: <strong>{item.status}</strong></div>
+                                <div>subregion: <strong>{item.subregion}</strong></div>
+                                <div>unMember:<strong>{item.unMember ? 'Yes' : 'No'}</strong></div>
+                            </div>
+                            <div className='c-2'>
+                                <div><strong>CoatOfArms</strong></div>
+                                <div className='coa'><img src={item.coatOfArms.png} alt="" /></div>
+                            </div>
+                        </div>
+                    </div>
+                </>
+            )
+        })
     )
 }
 
--- src/Home.js
+++ src/Home.js
@@ -1,15 +1,27 @@
 import React from 'react'
 import { useEffect, useState } from 'react'
 import './Home.css'
+import { Link } from 'react-router-dom';
 
 
 const Home = () => {
+
     const [data, setData] = useState([])
     const [firstTwnty, setfirstTwnty] = useState([])
+    const [search, setSearch] = useState('')
+    const [filteredData, setFilteredData] = useState([]);
+
     const getData = async () => {
         const result = await fetch('https://restcountries.com/v3.1/all')
         result.json().then(d => setData(d))
     }
+
+    const getSearchCountry = async () => {
+        const searchItem =
+            data.filter(item => item.name.common.toLowerCase().includes(search.toLowerCase()))
+        setFilteredData(searchItem);
+    }
+
     useEffect(() => {
         getData()
     }, [])
@@ -18,28 +30,63 @@ const Home = () => {
         setfirstTwnty(data.slice(0, 15));
     }, [data])
 
-    console.log(firstTwnty)
-    return (
-        <div className='home-cont'>
-            {firstTwnty ?
-                firstTwnty.map((item, index) => {
-                    const firstCurrency = Object.values(item.currencies)[0];
-                    const language = Object.values(item.languages)
-                    return (
-                        <div className='item'>
-                            <div className='flag-cont'><img src={item.flags.png} alt="" /></div>
-                            <div className='item-cont'>
-                                <div className='name'>Name: <strong>{item.name.common}</strong></div>
-                                <div className='continent'>Continent: <strong>{item.continents[0]}</strong></div>
-                                <div className='population'>Population: <strong>{item.population}</strong></div>
-                                <div className='currency'>Currency: <strong>{firstCurrency.name}</strong></div>
-                                <div className='lang'>Language: <strong>{language[0]}</strong></div>
-                            </div>
-                        </div>
+    useEffect(() => {
+        getSearchCountry()
+    }, [search, data])
 
-                    )
-                }) : ""
-            }
+    return (
+        <div className='main-cont'>
+            <div class="search-container">
+                <input onChange={(e) => setSearch(e.target.value)} type="text" class="search-input" placeholder="Search..." />
+                {/* <button class="search-button">Search</button> */}
+            </div>
+            <div className='home-cont'>
+                {(search && filteredData) ?
+                    filteredData.map((item, index) => {
+
+                        const currencies = item.currencies;
+                        const firstCurrency = currencies && typeof currencies === 'object' ? Object.values(currencies)[0] : null;
+
+                        const languages = item.languages;
+                        const language = languages ? Object.values(languages) : [];
+
+                        return (
+                            <Link to={`viewcountry/${item.name.common}`} style={{ textDecoration: 'none' }}>
+                                <div className='item'>
+                                    <div className='flag-cont'><img src={item.flags.png} alt="" /></div>
+                                    <div className='item-cont'>
+                                        <div className='name'>Name: <strong>{item.name.common}</strong></div>
+                                        <div className='continent'>Continent: <strong>{item.continents[0]}</strong></div>
+                                        <div className='population'>Population: <strong>{item.population}</strong></div>
+                                        <div className='currency'>Currency: <strong>{firstCurrency ? firstCurrency.name : ''}</strong></div>
+                                        <div className='lang'>Language: <strong>{language.length > 0 ? language[0] : ''}</strong></div>
+                                    </div>
+                                </div>
+                            </Link>
+
+                        )
+                    }) :
+                    firstTwnty.map((item, index) => {
+                        const firstCurrency = Object.values(item.currencies)[0];
+                        const language = Object.values(item.languages)
+                        return (
+                            <Link to={`viewcountry/${item.name.common}`} style={{ textDecoration: 'none' }}>
+                                <div className='item'>
+                                    <div className='flag-cont'><img src={item.flags.png} alt="" /></div>
+                                    <div className='item-cont'>
+                                        <div className='name'>Name: <strong>{item.name.common}</strong></div>
+                                        <div className='continent'>Continent: <strong>{item.continents[0]}</strong></div>
+                                        <div className='population'>Population: <strong>{item.population}</strong></div>
+                                        <div className='currency'>Currency: <strong>{firstCurrency.name}</strong></div>
+                                        <div className='lang'>Language: <strong>{language[0]}</strong></div>
+                                    </div>
+                                </div>
+                            </Link>
+
+                        )
+                    })
+                }
+            </div>
         </div>
     )
 }
